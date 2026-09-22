7ea18f4fa3da7b9a502b0192150e29ea437411f8
Chris <Christopher.cortes@inacapmail.cl>
2026-09-22 09:44:02 -0300

    "configuracion de django"

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,17 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
+DATABASES = {
+ 'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'mydatabase',
+    'USER': 'myuser',
+    'PASSWORD': 'mypassword',
+  }
+ }
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop',
 ]
 
 MIDDLEWARE = [
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    # Tipo 1: CharField (Cadenas de texto cortas)
+    nombre = models.CharField(max_length=100)
+    codigo_barras = models.CharField(max_length=50, unique=True)
+
+    # Tipo 2: TextField (Cadenas de texto largas / descripciones)
+    descripcion = models.TextField(blank=True, null=True)
+
+    # Tipo 3: IntegerField (Números enteros)
+    precio = models.IntegerField()  # Guardado en centavos o unidades enteras
+    stock = models.IntegerField(default=0)
+
+    # Tipo 4: DateTimeField (Fechas y horas)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
